8282ebd1e3000b389025648c3aa8a4626f2af64b
Helene Grini <helenegrini96@gmail.com>
2020-06-10 19:08:11 +0200

    Initialize firebase and add form endpoint

--- pages/api/form.js
+++ pages/api/form.js
@@ -1,8 +1,17 @@
-export default (req, res) => {
+import firebase from "../../firebase/config";
+
+export default async (req, res) => {
+  const forms = firebase.firestore().collection("forms");
   if (req.method === "POST") {
-    res.json({ method: "POST" });
+    const data = await forms
+      .add(JSON.parse(req.body))
+      .then((docRef) => docRef.get().then((doc) => doc.data()));
+    res.json(data);
   }
   if (req.method === "GET") {
-    res.json({ method: "GET" });
+    const data = await forms
+      .get()
+      .then((docRef) => docRef.get().then((doc) => doc.data()));
+    res.json(data);
   }
 };
--- pages/form.tsx
+++ pages/form.tsx
@@ -1,4 +1,7 @@
 const Form = () => {
+  /**
+   * Start writing your form in here
+   */
   return (
     <div className="container">
       <h1>Form</h1>
